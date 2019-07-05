900e67acf78b15973fd361a1a0874d1d952af506
Dmitry <d.xusnetd@yandex.ru>
2019-07-05 17:39:07 +0300

    project structure update

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,37 @@
+import pandas as pd
+from collections import defaultdict
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-login = LoginManager(app)
+excel_book = pd.ExcelFile("cleverdata_taxonomy_client.xlsm")
+attributes_taxonomy = excel_book.parse(sheet_name='Attributes', header=2, index=False, usecols=[1, 2, 3, 4, 5])
+# attribute_type = attributes_taxonomy[attributes_taxonomy["Short Name"] == "id"]["Type"].tolist()[0]
+dictionary_taxonomy = defaultdict(lambda: defaultdict())
+for sheet in excel_book.sheet_names[7:]:
+    data = excel_book.parse(sheet_name=sheet, index=False, header=2)
+    for i, line in data.iterrows():
+        dictionary_taxonomy[sheet][line.ID] = line.Description
 
-from app import routes, models
+source = pd.read_excel("source2.xlsx")
+source["id"] = source["id"].str.lower()
+
+# db = [{"id": "dmitry.khusnetdinov@sas.com",
+#        "attrs": [{"primary": 10055, "secondary": 10177}, {"primary": 10052, "secondary": 10004},
+#                  {"primary": 10057, "secondary": 10001}, {"primary": 10169, "secondary": 10003}]},
+#       {"id": "cidemo.cis@gmail.com",
+#        "attrs": [{"primary": 10055, "secondary": 10177}, {"primary": 10052, "secondary": 10007},
+#                  {"primary": 10057, "secondary": 10002}, {"primary": 10169, "secondary": 10001}]}
+#       ]
+
+# users = source[source["id"] == "Dmitry.Khusnedinov@sas.com"].to_dict(orient="records")
+# print()
+
+
+app = Flask(__name__)
+# app.config.from_object(Config)
+# db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
+
+from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,29 +1,5 @@
-import pandas as pd
-from collections import defaultdict
-from flask import Flask, jsonify, abort, make_response, request
-
-excel_book = pd.ExcelFile("cleverdata_taxonomy_client.xlsm")
-attributes_taxonomy = excel_book.parse(sheet_name='Attributes', header=2, index=False, usecols=[1, 2, 3, 4, 5])
-# attribute_type = attributes_taxonomy[attributes_taxonomy["Short Name"] == "id"]["Type"].tolist()[0]
-dictionary_taxonomy = defaultdict(lambda: defaultdict())
-for sheet in excel_book.sheet_names[7:]:
-    data = excel_book.parse(sheet_name=sheet, index=False, header=2)
-    for i, line in data.iterrows():
-        dictionary_taxonomy[sheet][line.ID] = line.Description
-
-source = pd.read_excel("source2.xlsx")
-source["id"] = source["id"].str.lower()
-
-# db = [{"id": "dmitry.khusnetdinov@sas.com",
-#        "attrs": [{"primary": 10055, "secondary": 10177}, {"primary": 10052, "secondary": 10004},
-#                  {"primary": 10057, "secondary": 10001}, {"primary": 10169, "secondary": 10003}]},
-#       {"id": "cidemo.cis@gmail.com",
-#        "attrs": [{"primary": 10055, "secondary": 10177}, {"primary": 10052, "secondary": 10007},
-#                  {"primary": 10057, "secondary": 10002}, {"primary": 10169, "secondary": 10001}]}
-#       ]
-
-# users = source[source["id"] == "Dmitry.Khusnedinov@sas.com"].to_dict(orient="records")
-# print()
+from app import app, attributes_taxonomy, dictionary_taxonomy, source
+from flask import jsonify, abort, make_response, request
 
 values_type = ["Boolean", "Character", "Byte", "Integer",
                "Long", "Double", "String", "Enum", "ABoolean",
@@ -55,21 +31,6 @@ def encode_data(user):
     return coded_data
 
 
-# users = source[source["id"] == "Dmitry.Khusnetdinov@sas.com"].to_dict(orient='records')
-# if (len(users) != 0):
-#     coded_data = encode_data(users[0])
-
-app = Flask(__name__)
-
-
-# @app.route('/fakeDMP/api/v1.0/users', methods=['GET'])
-# def get_user_info():
-#     users = list(filter(lambda u: u['id'] == request.args.get("id"), source["id"]))
-#     if (len(users) == 0):
-#         abort(404)
-#
-#     return jsonify(users[0])
-
 @app.route('/fakeDMP/api/v1.0/users', methods=['GET'])
 def get_user_info():
     users = source[source["id"] == request.args.get("id")].to_dict(orient='records')
@@ -83,7 +44,3 @@ def get_user_info():
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'not found'}), 404)
-
-
-if (__name__ == '__main__'):
-    app.run(debug=True)
